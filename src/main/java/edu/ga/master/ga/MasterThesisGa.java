a67e3db087b82404a9e9da62a7d1a613b217e9e4
Luca <luca.coraci@gmail.com>
2022-11-18 16:33:16 +0100

    added ga dependency

--- src/main/java/edu/ga/master/ga/MasterThesisGa.java
+++ src/main/java/edu/ga/master/ga/MasterThesisGa.java
@@ -2,8 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package edu.ga.master.ga;
+
+import org.jenetics.BitChromosome;
+import org.jenetics.BitGene;
+import org.jenetics.Genotype;
+import org.jenetics.engine.Engine;
+import org.jenetics.util.Factory;
 
 /**
  *
@@ -14,6 +19,14 @@ public class MasterThesisGa {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Ciao Luca come va tutt'appost ?");
+
+        Factory<Genotype<BitGene>> gtf = Genotype.of(BitChromosome.of(10, 0.5));
+
+     
+    }
+
+    public Integer eval(Genotype<BitGene> gt) {
+        return gt.getChromosome().as(BitChromosome.class).bitCount();
     }
 }
 //prova
